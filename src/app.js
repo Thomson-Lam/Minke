087c802dc2199ea100b2c455206851937a9c66b1
Thomson-Lam <145819823+Thomson-Lam@users.noreply.github.com>
2024-10-18 22:51:08 -0400

    chore: added app.js

---
+++ src/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Component from './components/Bars';
+
+export default function App () {
+
+    return (
+        <>
+        <Component />
+        </>
+    );
+}
