669cc555cbab7518a54c7d96275d03b6e2f7b55e
twang849 <163226113+twang849@users.noreply.github.com>
2024-10-19 17:28:00 -0400

    Update feedbutton.js

--- src/components/feedbutton.js
+++ src/components/feedbutton.js
@@ -1,8 +1,9 @@
 import React, { useState} from 'react'
 import './feedbutton.css'
+import feedpic from './feed.jpg'
 
-const FeedButton = () => {
-    const [hunger, setHunger] = useState(0);
+export const FeedButton = () => {
+    var [hunger, setHunger] = useState(0);
 
     function handleFeed() {
         alert('Fed pet! ');
@@ -11,8 +12,8 @@ const FeedButton = () => {
     
     return (
         <div>
-            <button onClick={handleFeed}>
-                <img src='https://media.istockphoto.com/id/901501348/vector/slice-of-melted-cheese-pepperoni-pizza.jpg?s=612x612&w=0&k=20&c=0cIsVfLHiabGD1NGDZEfE1hIBmD5DQuzNuLK8Owvyo8=' alt='Feed img' className='pizza'/>
+            <button onClick={handleFeed} className='button'>
+                {/* <img src={feedpic} alt='Feed img' className='pizza'/> */}
             </button>
             <br />
             <br />
@@ -22,4 +23,5 @@ const FeedButton = () => {
   
 }
 
-export default FeedButton
+export var hunger;
+
