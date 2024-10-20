663b546ac171cecf62df875bc66060908991f9d8
twang849 <163226113+twang849@users.noreply.github.com>
2024-10-20 11:38:46 -0400

    Fix

--- src/components/DogImage.js
+++ src/components/DogImage.js
@@ -4,24 +4,12 @@ import happy_dog from '../Assets/happy_dog.png'
 
 const DogImage = ({hungerValue}) => {
     var dog_img;
-//    const [key, setKey] = useState(0);
-
-//    function refresh () {
-//         setKey(key + 1);
-//    }
-
-//     if ({hungerValue} < 50) {
-//         dog_img = <img src={sad_dog} alt='sad_dog.png' />
-//     } else {
-//         dog_img = <img src={happy_dog} alt='happy_dog.png' />
-//     }
 
     return (
         <div className='dog'>
             { hungerValue < 50 ? <img src={sad_dog} alt='sad_dog.png'/> : <img src={happy_dog} alt='happy_dog.png' />}
             {dog_img}
             <br />
-            DogImage hungerValue = {hungerValue}
         </div>
     )
 }
